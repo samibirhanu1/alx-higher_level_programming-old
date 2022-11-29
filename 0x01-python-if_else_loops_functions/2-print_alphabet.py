207392017d9338ab246c6871db475bdaa8d4666f
samibirhanu1 <samuelbirhanu01@gmail.com>
2022-11-29 10:53:00 -0800

    correction

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Sami Birhanu <samuelbirhanu01@gmail.com>
 
 """Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 4-print_hexa.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Sami Birhanu <samuelbirhanu01@gmail.com>
 
 """Print numbers 0 to 98 in decimal and hexadecimal."""
 for number in range(0, 99):
